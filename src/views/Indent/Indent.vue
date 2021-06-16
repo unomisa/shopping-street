d18ac48c6043587bd596c6ee4e04dbe030c0cad7
unomisa <1415769005@qq.com>
2021-06-16 21:05:17 +0800

    功能完善，及我的页面基本搭建

---
+++ src/views/Indent/Indent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <van-nav-bar title="确认订单"
+                 left-text="返回"
+                 left-arrow
+                 @click-left="onClickLeft" />
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+</style>
